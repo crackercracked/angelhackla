6220c4681964a9ec2b7c309ebf52a3923127d0e2
Your Name <you@example.com>
2018-06-15 23:28:46 -0700

    add google cloud client lib

--- src/index.js
+++ src/index.js
@@ -4,12 +4,57 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-//app.get('/', (req, res) => {
-  //res.send("Hello world")
-//});
-
 app.use(express.static(__dirname + '/' + '../public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
+
+const record = require('node-record-lpcm16');
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+// Creates a client
+const client = new speech.SpeechClient();
+
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'en-US';
+
+// audio recognize
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  interimResults: false, // If you want interim results, set this to true
+};
+
+// Create a recognize stream
+const recognizeStream = client
+  .streamingRecognize(request)
+  .on('error', console.error)
+  .on('data', data =>
+    process.stdout.write(
+      data.results[0] && data.results[0].alternatives[0]
+        ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+        : `\n\nReached transcription time limit, press Ctrl+C\n`
+    )
+  );
+
+// Start recording and send the microphone input to the Speech API
+record
+  .start({
+    sampleRateHertz: sampleRateHertz,
+    threshold: 0,
+    // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+    verbose: false,
+    recordProgram: 'rec', // Try also "arecord" or "sox"
+    silence: '10.0',
+  })
+  .on('error', console.error)
+  .pipe(recognizeStream);
+
+console.log('Listening, press Ctrl+C to stop.');
+
+
